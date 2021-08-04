8b683223f38b8c3cb996ae4e5a0fe224237f2e48
Sibylle Eloy <sibylleeloy@hotmail.com>
2021-08-04 15:03:25 +0200

    fix: static cart dropdown

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,5 +1,9 @@
-// function cartOpen() {
-//   let element = document.getElementById("dropdownCart");
+function cartOpen() {
+  let cart = document.getElementById("dropdownCart");
+  console.log(cart);
+  let body = document.getElementById("body");
+  console.log(body);
 
-//   element.classList.toggle("cartOpened");
-// }
+  cart.classList.add("cartOpened");
+  body.classList.add("cartOpened");
+}
